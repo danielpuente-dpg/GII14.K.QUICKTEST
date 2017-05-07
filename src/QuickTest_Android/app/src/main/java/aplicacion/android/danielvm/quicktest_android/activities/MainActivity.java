67368db7e981a84f4c110f02530a919efbb4d573
Daniel <danielpuente7.dp@gmail.com>
2017-05-07 21:00:07 +0200

    #30 Se ha añadido un boton de opciones, Navigation Drawer, desde el cual se permite que el usuario pueda cerrar sesión o cerrar sesión y olvidar. Además, se incluye brevemente un apartado de ayuda de la app que posteriormente se desarrollara.

--- src/QuickTest_Android/app/src/main/java/aplicacion/android/danielvm/quicktest_android/activities/MainActivity.java
+++ src/QuickTest_Android/app/src/main/java/aplicacion/android/danielvm/quicktest_android/activities/MainActivity.java
@@ -1,15 +1,27 @@
 package aplicacion.android.danielvm.quicktest_android.Activities;
 
+import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -19,17 +31,25 @@ import java.util.concurrent.ExecutionException;
 import aplicacion.android.danielvm.quicktest_android.API.APIMoodle;
 import aplicacion.android.danielvm.quicktest_android.API.APIServices.MoodleService;
 import aplicacion.android.danielvm.quicktest_android.Adapters.PagerAdapter;
+import aplicacion.android.danielvm.quicktest_android.Fragments.CuestionarioFragment;
+import aplicacion.android.danielvm.quicktest_android.Fragments.SecondFragment;
+import aplicacion.android.danielvm.quicktest_android.Fragments.ThirdFragment;
 import aplicacion.android.danielvm.quicktest_android.Models.Moodle.Content;
 import aplicacion.android.danielvm.quicktest_android.Models.Moodle.Course;
 import aplicacion.android.danielvm.quicktest_android.Models.Moodle.Module;
 import aplicacion.android.danielvm.quicktest_android.Models.Moodle.Token;
 import aplicacion.android.danielvm.quicktest_android.Models.Moodle.User;
 import aplicacion.android.danielvm.quicktest_android.R;
+import aplicacion.android.danielvm.quicktest_android.Utils.Util;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    // Atributos del Navigation Drawer
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
@@ -57,41 +77,142 @@ public class MainActivity extends AppCompatActivity {
 
         prefs = getSharedPreferences("Preferences", Context.MODE_PRIVATE);
 
-        //////////////////////////////////////////////////
-        // Obtenemos el token del usuario que usara durante la sesion
+        // Recuperamos la informacion del login
         getTokenUser();
-        // Obtenemos el nombre del usuario
         getNameUser();
-        //////////////////////////////////////////////////
 
-        // Obtenemos el token del usuario encargado de interactuar con el WebServices
+        // Obtenemos el numero de herramientas externas y el token del usuario encargado de interactuar con el WebServices
         getTokenWS();
-        // Obtenemos la informacion del usuario
         getUserField();
-        // Obtenemos los cursos en los que ese usuario se encuentra matriculado
         getEnrolUserCourse(user.getId());
-
-        // Obtenemos los cursos de tipo lti
         getCoursesLTI();
-
-
-        // Obtenemos todos los cursos
         getNumberOfCourses();
-
-        Log.d("Modules2", modules.size() + "");
-        // Obtenemos el numero de herramientas externas de tipo lti
         getNumberExternalTools();
-        Log.d("Modules3", modules.size() + "");
-
 
         setToolbar();
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.navview);
+
+        // Obtenemos la vista del Navigation Drawer y modificamos los campos
+        // en funcion del usuario
+        View header = navigationView.getHeaderView(0);
+        TextView user = (TextView)header.findViewById(R.id.textView);
+        user.setText(NAME);
+
         // Preparamos el Layout del Tab
         setTabLayout();
         setViewPager();
         // Logica del Tab al realizar una accion
         setListenerTabLayout(viewPager);
 
+
+
+        // Evento Navigation Drawer
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.menu_info:
+                        // TODO gotoHelpActivity
+                        break;
+
+                    case R.id.menu_log_out:
+                        logOut();
+                        break;
+
+                    case R.id.menu_forget_and_log_out:
+                        Util.removeUserToSharedPreferences(prefs);
+                        logOut();
+                        break;
+                }
+
+                return true;
+            }
+        });
+
     }
+
+    private void logOut() {
+        Intent intentLogin = new Intent(this, LoginActivity.class);
+
+        // Al hacer clicked Atras, cerramos la App
+        intentLogin.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        startActivity(intentLogin);
+    }
+
+    private void setToolbar() {
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    private void setTabLayout() {
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Cuestionarios"));
+        tabLayout.addTab(tabLayout.newTab().setText("Resultados"));
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+    }
+
+    private void setViewPager() {
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        // Cada vez que cambiemos de Tabs, el viewPager (encargado de los fragments )tambien lo cambie
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+    }
+
+    private void setListenerTabLayout(final ViewPager viewPager) {
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            // Al seleccionar el Tab
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                viewPager.setCurrentItem(position);
+            }
+
+            // Cuando el Tab activo deja de estarlo
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            // Cuando seleccionamos el mismo tab que esta activo
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    // Metodo encargado de enlazar el boton del burger menu con el evento
+    // del navigation drawer.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // Abrimos el menu lateral
+                // La opcion start, esque lo tenemos a la parte izquierda
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    /*
+
+    Logica del Webservices Moodle
+
+
+    */
 
     private void getTokenWS() {
         TokenUserWSRequest tokenUserWSRequest = new TokenUserWSRequest(APIMoodle.getApi());
@@ -190,56 +311,6 @@ public class MainActivity extends AppCompatActivity {
         bundle = getIntent().getExtras();
         NAME = bundle.getString("name");
         Log.d("NAME", NAME);
-    }
-
-    private void setToolbar() {
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(myToolbar);
-    }
-
-    private void setTabLayout() {
-        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
-        //tabLayout.addTab(tabLayout.newTab().setIcon(R.mipmap.ic_icon_tag_cuestionario));
-        tabLayout.addTab(tabLayout.newTab().setText("Cuestionarios"));
-        tabLayout.addTab(tabLayout.newTab().setText("Resultados"));
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-    }
-
-    private void setViewPager() {
-        viewPager = (ViewPager) findViewById(R.id.viewPager);
-        adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
-        viewPager.setAdapter(adapter);
-        // Cada vez que cambiemos de Tabs, el viewPager (encargado de los fragments )tambien lo cambie
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-    }
-
-    private void setListenerTabLayout(final ViewPager viewPager) {
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            // Al seleccionar el Tab
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                int position = tab.getPosition();
-                viewPager.setCurrentItem(position);
-            }
-
-            // Cuando el Tab activo deja de estarlo
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            // Cuando seleccionamos el mismo tab que esta activo
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return super.onCreateOptionsMenu(menu);
     }
 
 
